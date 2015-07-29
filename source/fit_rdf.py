6b7889219d8732c94ea4eca275dee3d67814e8ac
Kevin Whitham <kw242@cornell.edu>
2015-07-29 10:41:42 -0400

    Output plot formatting changes. Prettier plot settings using Seaborn, prettyplotlib, changed labels.

--- source/fit_rdf.py
+++ source/fit_rdf.py
@@ -3,7 +3,11 @@
 # points and fitting the rdf to a model to quantify disorder
 
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import plotsettings
+import prettyplotlib as ppl
+import seaborn as sns
 from paircorrelation import PairCorrelationFunction_2D
 from paircorrelation import fit_hex_paracrystal_rdf, fit_square_paracrystal_rdf, fit_hex_disordered_rdf, fit_square_disordered_rdf
 from paircorrelation import generate_paracrystal_rdf, generate_disordered_rdf
@@ -65,6 +69,17 @@ popt, pcov = curve_fit(fit_function, r, g, p0=params)
 print(popt)
 print(pcov)
 
+# fit results
+# a is the length of the first basis vector
+# b is the length of the second basis vector
+# popt[0] is the std. dev. 
+
+# DEBUG!!!
+# you can force the values here instead of using the fit value
+#print('Overriding fit results!')
+#popt[1] = 6.61;
+#popt[0] = 0.035*popt[1]
+
 if args.symmetry == 4:
     a=(popt[1],0.0)
     b=(0.0,popt[1])
@@ -73,19 +88,42 @@ elif args.symmetry == 6:
     b=(popt[1]*-0.5,popt[1]*np.sqrt(3.0)/2.0)
 
 if args.model == 'uniform':
+    full_model_string = 'Uncorrelated Disorder'
     r_fit, g_fit = generate_disordered_rdf(a, b, popt[0], max_distance=np.max(r), resolution=r[1]-r[0])
     func_label = r'$\sum_i\/\frac{1}{2\pi r_i \sigma} exp\left(\frac{-(r-r_i)^2}{2 \sigma^2}\right)$'
 elif args.model == 'para':
+    full_model_string = 'Paracrystal'
     r_fit, g_fit = generate_paracrystal_rdf(a, b, popt[0], max_distance=np.max(r), resolution=r[1]-r[0])
     func_label = r'$\sum_i\/\frac{L}{2\pi r_i^2 \sigma} exp\left(\frac{-(r-r_i)^2}{2 (\sigma r_i/L)^2}\right)$'
 else:
     print('Model not recognized')
-    
+
+pub = plotsettings.Set('Nature')
+pub.set_figsize(n_columns = 1, n_rows = 1)
+
 plt.figure(1)
-plt.plot(r,g,'r-',label='Data')
-plt.plot(r_fit, g_fit, 'k-', label=func_label+' $\sigma$ = '+'%(std).2f%%, L = %(L).2f nm' % {'std':popt[0]/popt[1]*100.0, 'L':popt[1]})
-plt.xlabel('$r (nm)$')
-plt.ylabel('$g(r)$')
+#plt.plot(r,g,'r-',label='Data')
+#plt.plot(r_fit, g_fit, 'k-', label=func_label+' $\sigma$ = '+'%(std).2f%%, L = %(L).2f nm' % {'std':popt[0]/popt[1]*100.0, 'L':popt[1]})
+
+sns.set_style("white")
+sns.despine()
+
+# make text editable in .pdf files
+mpl.rc('pdf', fonttype=42)
+
+ppl.plot(r,g,label='Data')
+#ppl.plot(r_fit, g_fit, label=func_label+' $\sigma$ = '+'%(std).2f%%, L = %(L).2f nm' % {'std':popt[0]/popt[1]*100.0, 'L':popt[1]})
+ppl.plot(r_fit, g_fit, label=full_model_string)
+plt.xlabel('r (nm)')
+plt.ylabel('g(r)')
+
+mpl.rc('pdf', fonttype=3)
+
 plt.gca().legend()
-plt.savefig(output_data_path+filename+'_'+args.model+'_'+symmetry_label+'_RDF.png', bbox_inches='tight')
-np.savetxt(output_data_path+filename+'_'+args.model+'_'+symmetry_label+'_RDF.txt',zip(r,g),header='r (nm)\tg(r)')
+plt.savefig(output_data_path+filename+'_'+args.model+'_'+symmetry_label+'_RDF.pdf', bbox_inches='tight')
+
+header_string = 'Radial distribution function\n'
+header_string += 'Model: '+args.model+' Equation: '+func_label+'\n'
+header_string += 'sigma = '+'%(std).2f%%, L = %(L).2f nm\n' % {'std':popt[0]/popt[1]*100.0, 'L':popt[1]}
+header_string += 'r (nm)\tg(r)'
+np.savetxt(output_data_path+filename+'_'+args.model+'_'+symmetry_label+'_RDF.txt',zip(r,g),header=header_string)
